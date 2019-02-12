ba99cbbcf8a410c2ebcb25af45adfc6b4bd29519
xinlong.zhang <xinlong.zhang@eoitek.com>
2019-02-12 19:32:10 +0800

    delete 0.0.2 code

--- app/c_app4/hello4.c
+++
@@ -1,6 +0,0 @@
-void api_putstr0(char *);
-
-void main(void)
-{
-    api_putstr0("hello, world\n");
-}
--- app/c_app5/winhello.c
+++
@@ -1,9 +0,0 @@
-int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
-
-char buf[150*50];
-
-void main(void)
-{
-    int win;
-    win = api_openwin(buf, 150, 50, -1, "hello");
-}
